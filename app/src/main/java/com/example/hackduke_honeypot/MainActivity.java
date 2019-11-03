02ac8c71c6425305ccd6e3ca22a7fc32481fec34
Aquisense-Nick <nick.maleki@aquisense.com>
2019-11-03 08:38:30 -0500

    alot

--- app/src/main/java/com/example/hackduke_honeypot/MainActivity.java
+++ app/src/main/java/com/example/hackduke_honeypot/MainActivity.java
@@ -6,12 +6,25 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TypefaceSpan;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -21,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    Button bMessage;
+    FloatingActionButton fab;
+    FloatingActionButton fab2;
+    String macBT;
+
 
     ArrayList<Message> messages = new ArrayList<>();
     static final int REQ_CODE = 100;
@@ -29,11 +45,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("honeypot");
 
-        bMessage = findViewById(R.id.bMessage);
+
+
+        //SpannableString s = new SpannableString("honeypot");
+        // typeface = Typeface.createFromAsset(getAssets(), "fonts/baloo_regular.ttf");
+        //s.setSpan(new TypefaceSpan(typeface), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //setTitle(s);
 
         messages.add(new Message("1", "The police are attacking rioters", "1:00"));
         messages.add(new Message("2", "Free Hong Kong", "2:00"));
@@ -48,21 +71,56 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MessageAdapter(messages);
         mRecyclerView.setAdapter(mAdapter);
 
-        bMessage.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PostMessageActivity.class);
-                startActivity(intent);
+                startActivityForResult(new Intent(MainActivity.this, PostMessageActivity.class), REQ_CODE);
             }
         });
+
+
+
+        FloatingActionButton fabBT = findViewById(R.id.fabBT);
+        fabBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Bluetooth Mac Address");
+                builder.setMessage("The hardware Bluetooth address is needed to send and receive messages over Bluetooth. Copy this from Android Settings -> System -> About Phone -> Status -> Bluetooth address.");
+                final EditText input = new EditText(MainActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(new EditText(MainActivity.this));
+
+                // Set up the buttons
+                builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        macBT = input.getText().toString();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-////        if (resultCode == RESULT_OK) {
-////            Message message = data.getParcelableExtra(MESSAGE_DATA);
-////            messages.add(message);
-////        }
-//    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            Message message = data.getParcelableExtra(MESSAGE_DATA);
+            messages.add(message);
+        }
+
+    }
 }
--- app/src/main/java/com/example/hackduke_honeypot/PostMessageActivity.java
+++ app/src/main/java/com/example/hackduke_honeypot/PostMessageActivity.java
@@ -4,31 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.format.DateFormat;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Date;
+import android.widget.ImageView;
 
 public class PostMessageActivity extends AppCompatActivity {
     Button bCancel, bPost;
-    EditText etMessageData;
+    EditText getMessageData;
+
+    ImageView ivBack;
+    ImageView ivPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_message);
 
-        bCancel = findViewById(R.id.bCancel);
-        bPost = findViewById(R.id.bPost);
-        etMessageData = findViewById(R.id.etMessageData);
+        //bCancel = findViewById(R.id.bCancel);
+        //bPost = findViewById(R.id.bPost);
+        getMessageData = findViewById(R.id.getMessageData);
+        ivBack = findViewById(R.id.ivBack);
 
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        ivPost = findViewById(R.id.ivPost);
+
+        ivPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message message = new Message("3", getMessageData.getText().toString(), "9");
+                Intent intent = new Intent(PostMessageActivity.this, MainActivity.class);
+                intent.putExtra(MainActivity.MESSAGE_DATA, message);
+                setResult(MainActivity.REQ_CODE, intent);
+                //startActivity(intent);
+                finish();
+            }
+        });
 //        Date date = new Date();
 //        CharSequence time  = DateFormat.format("EEEE, MMMM d, yyyy ", date.getTime());
-        Message message = new Message("3", etMessageData.getText().toString(), "9");
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(MainActivity.MESSAGE_DATA, message);
-        finish();
+
+
     }
+
 }
