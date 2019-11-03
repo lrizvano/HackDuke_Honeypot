64c3543f8ce7e35d80c070c4701b7da11d3af8ac
John Garrett <jacgarrett18@gmail.com>
2019-11-03 10:07:33 -0500

    table view completion

--- honeypot-ios/honeypot-ios/FeedViewController.swift
+++ honeypot-ios/honeypot-ios/FeedViewController.swift
@@ -1,17 +1,30 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var tableView: UITableView!
     var messages = [Message]() {
         didSet {
             DispatchQueue.main.async {
-                self.collectionView.reloadData()
+                self.tableView.reloadData()
             }
         }
     }
-    
+
     override func viewDidLoad() {
+        if traitCollection.userInterfaceStyle == .light {
+            view.backgroundColor = UIColor(displayP3Red: 1, green: 248.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+            tableView.backgroundColor = UIColor(displayP3Red: 1, green: 248.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+        }
+        
         super.viewDidLoad()
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "header"))
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 60
+
+
         HoneypotAPI.getMessages { result in
             switch result {
             case .success(let messages):
@@ -23,24 +36,38 @@ class FeedViewController: UIViewController {
     }
 }
 
-extension FeedViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 60)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feedCell", for: indexPath) as? FeedCollectionViewCell else {
-            return UICollectionViewCell()
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as? FeedTableViewCell else {
+            return UITableViewCell()
         }
         
         cell.lblMessage.text = messages[indexPath.item].msg
         cell.lblTime.text = messages[indexPath.item].time
         cell.lblId.text = messages[indexPath.item].id
-        
+        if traitCollection.userInterfaceStyle == .light {
+            cell.lblMessage.textColor = .black
+            cell.lblTime.textColor = .gray
+            cell.lblId.textColor = .lightGray
+        } else {
+            cell.lblMessage.textColor = .black
+            cell.lblTime.textColor = .gray
+            cell.lblId.textColor = .lightGray
+        }
+        cell.layer.cornerRadius = 14.0
         return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if traitCollection.userInterfaceStyle == .light {
+            cell.backgroundColor = UIColor(displayP3Red: 246.0/255.0, green: 214.0/255.0, blue: 50.0/255.0, alpha: 1.0)
+        } else {
+            cell.backgroundColor = UIColor(displayP3Red: 246.0/255.0, green: 214.0/255.0, blue: 50.0/255.0, alpha: 0.75)
+        }
     }
 }
